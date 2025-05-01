c9ce9786b1bf73967cb0ffef514cc9ac2a32a1a7
Oosaka <davide.vailati03@gmail.com>
2025-05-01 11:51:42 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export type EntityFile = {
+  format_version:string;
+  "minecraft:entity":Entity;
+}
+export type Entity = {
+  description:EntityDescription;
+  components:ComponentGroup;
+}
+
+export type EntityDescription = {
+  identifier:string;
+  is_spawnable:boolean;
+  is_summonable:boolean;
+  is_experimental:boolean;
+}
+
+export type ComponentGroup = {
+  "minecraft:type_family"?:TypeFamilyComponent;
+}
+
+export type TypeFamilyComponent = {
+  family: string[];
+}
